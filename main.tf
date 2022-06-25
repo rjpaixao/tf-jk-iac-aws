5438891fad91adbd93b33b33c13179c9bbbd0af9
Rogerio de Jose Paixao <rogerio.j.paixao@gmail.com>
2022-06-25 18:59:53 -0300

    Config da Infra

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.19.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+}
+
+# Configuração do Módulo VPC
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.2"
+
+  name                = var.vpc_name
+  cidr                = var.vpc_cidr
+  azs                 = var.vpc_azs
+  private_subnets     = var.vpc_private_subnets
+  public_subnets      = var.vpc_public_subnets  
+  enable_nat_gateway  = var.vpc_enable_nat_gateway
+  tags                = var.vpc_tags  
+}
+
+# Criação da Instância EC2
+
+module "ec2-instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "4.0.0"
+  
+  for_each = toset(["one", "two", "three"])
+
+  name = "instance-${each.key}"
+
+  ami                    = "ami-08d4ac5b634553e16"
+  instance_type          = "t2.micro"
+  key_name               = "DevOps"
+  monitoring             = true
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
